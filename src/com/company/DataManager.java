a7ae182f4d97e1a51c6d0ce36c57a61c713fb76d
george <mai1428@uom.edu.gr>
2016-12-10 18:44:28 +0200

    begin

---
+++ src/com/company/DataManager.java
@@ -0,0 +1,14 @@
+package com.company;
+
+/**
+ * Created by george on 9/12/2016.
+ */
+import weka.core.Instances;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class DataManager {
+
+
+
+}
---
+++ src/com/company/J48Runner.java
@@ -0,0 +1,15 @@
+package com.company;
+
+/**
+ * Created by george on 9/12/2016.
+ */
+import weka.core.Instances;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class J48Runner {
+
+
+
+
+}
